cb641245291ad139bac6815eb63c1943f0c8c6b2
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-08-05 14:15:12 +0300

    Add openHeartBankGUI method

--- src/main/java/org/strassburger/lifestealz/util/GuiManager.java
+++ src/main/java/org/strassburger/lifestealz/util/GuiManager.java
@@ -1,15 +1,19 @@
 package org.strassburger.lifestealz.util;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.strassburger.lifestealz.LifeStealZ;
 import org.strassburger.lifestealz.util.customitems.CustomItemManager;
+import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.List;
@@ -62,5 +66,33 @@ public class GuiManager {
 
         if (addNextButton) inventory.setItem(52, CustomItemManager.createNextItem(page + 1));
         else inventory.setItem(52, glass);
+    }
+
+    private static void openHeartBankGUI(Player player) {
+        Inventory inventory = Bukkit.createInventory(player, 9, MessageUtils.getAndFormatMsg(false, "messages.heartBankTitle", "&8Heart Bank"));
+        MiniMessage miniMessage = MiniMessage.miniMessage();
+
+        ItemStack glass = new ItemStack(Material.GRAY_STAINED_GLASS_PANE, 1);
+
+        // Set the display name of the glass to an empty string
+        ItemMeta glassMeta = glass.getItemMeta();
+        glassMeta.displayName(Component.text(" "));
+        glass.setItemMeta(glassMeta);
+
+        Component centerItemName = Component.text("&cYou currently have X amount of hearts in your heart bank.");
+
+        ItemStack centerItem = new ItemStack(Material.RED_DYE, 1);
+        ItemMeta centerItemMeta = centerItem.getItemMeta();
+        centerItemMeta.addEnchant(Enchantment.MENDING, 1, true);
+        centerItemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        centerItemMeta.displayName(centerItemName);
+
+        inventory.setItem(4, centerItem);
+        for (int i = 0; i < 9; i++) {
+            if (i == 4) continue;
+            inventory.setItem(i, glass);
+        }
+
+        player.openInventory(inventory);
     }
 }
