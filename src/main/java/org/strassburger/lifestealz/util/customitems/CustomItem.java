6d2a76339609ebc40e805adc0f4a8449789ebb49
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-08-11 18:38:54 +0300

    remove whitepsace

--- src/main/java/org/strassburger/lifestealz/util/customitems/CustomItem.java
+++ src/main/java/org/strassburger/lifestealz/util/customitems/CustomItem.java
@@ -8,7 +8,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.strassburger.lifestealz.util.MessageUtils;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
