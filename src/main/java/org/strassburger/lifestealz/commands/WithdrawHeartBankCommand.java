372dd5e7c01df5c6cd5eeef2b33a8f99c7d28a83
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-08-11 18:42:55 +0300

    Create WithdrawHeartBankCommand class

---
+++ src/main/java/org/strassburger/lifestealz/commands/WithdrawHeartBankCommand.java
@@ -0,0 +1,14 @@
+package org.strassburger.lifestealz.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class WithdrawHeartBankCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+		return true;
+	}
+}
